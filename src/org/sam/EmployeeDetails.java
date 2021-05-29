b3f6fb67b146707ec703d21e2b462ebd644a4d9f
syedfarith127 <syedfarith31@gmail.com>
2021-05-29 23:49:34 +0530

    uvaish updated copy push

--- src/org/sam/EmployeeDetails.java
+++ src/org/sam/EmployeeDetails.java
@@ -4,10 +4,12 @@ public class EmployeeDetails {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("cat");
 		System.out.println("php");
 		System.out.println("Java");
-		System.out.println("python");
 		System.out.println("JavaSctipt");
+		System.out.println("python");
+		System.out.println("lion");
 
 		
 	
